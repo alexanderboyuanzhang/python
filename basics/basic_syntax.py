732090f89e4153c56c1abca85f3d8e561b32c7d4
alexanderboyuanzhang <alexander.boyuan.zhang@gmail.com>
2019-10-19 12:23:43 +0200

    .

--- basics/basic_syntax.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-print ("Please refer to https://www.tutorialspoint.com/python3/python_basic_syntax.htm")
--- basics/faq.py
+++
@@ -1,7 +0,0 @@
-'''
-Print new output on same line
-'''
-print("-------------------------------\nPrint new output on same line\n---")
-for i in range(1,11):
-	print(i,end=' ')
-print()
